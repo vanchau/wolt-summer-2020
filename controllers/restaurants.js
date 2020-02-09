bdad9c618b64f38f68b9012142e5e2c1a6330f8b
vanchau <van.chau@aalto.fi>
2020-02-09 19:02:00 +0200

    refactor and fix bugs

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,41 +1,11 @@
 const restaurantsRouter = require('express').Router()
 const { query, validationResult } = require('express-validator')
 const restaurants = require('../data/restaurants.json').restaurants
-const haversine = require('haversine-distance')
-
-const filterRestaurants = (q, lat, lon) => {
-
-  q = q.replace(/\s+/g,' ') // remove extra whitespace between words
-    .trim()                 // remove extra whitespace before and after the string
-    .split(' ')             // split into array of words for partial matches
-
-  // Helper function: Check for full or partial matches in name, description or tags
-  const queryMatch = (restaurant) => {
-    if (q.some(word => restaurant.name.includes(word)
-        || restaurant.description.includes(word)
-        || restaurant.tags.includes(word))) {
-
-      return true
-    }
-  }
-
-  // Helper function: Distance calculated with Haversine formula
-  const distanceMatch = (restaurant) => {
-    const restaurantLocation = { lat: restaurant.location[1], lon: restaurant.location[0] }
-    const currentLocation = { lat: lat, lon: lon }
-
-    if (haversine(restaurantLocation, currentLocation) < 3000) {
-      return true
-    }
-  }
-
-  // Filter and return restaurants
-  return restaurants.filter(restaurant => queryMatch(restaurant) && distanceMatch(restaurant))
-}
+const utils = require('../utils/utils')
 
 restaurantsRouter.get('/search', [
   query('q')
-    .exists().withMessage('Undefined query string.'),
+    .notEmpty().withMessage('Undefined query string.'),
   query('lat')
     .exists().withMessage('Undefined latitude.')
     .isFloat({ min: -90, max: 90 }).withMessage('Invalid latitude format.'),
@@ -49,9 +19,8 @@ restaurantsRouter.get('/search', [
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() })
   }
-
   // Return valid restaurants
-  res.json(filterRestaurants(req.query.q, req.query.lat, req.query.lon))
+  res.json(utils.filterRestaurants(restaurants, req.query.q, req.query.lat, req.query.lon))
 })
 
 module.exports = restaurantsRouter
